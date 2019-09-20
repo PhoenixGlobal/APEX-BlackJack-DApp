08f46f321d75cc8ebe3a751f5f0067d573842578
Artem <ae@atr0x.com>
2019-09-20 10:01:04 +0200

    reduce sleep

--- src/main/java/app/controller/MainController.java
+++ src/main/java/app/controller/MainController.java
@@ -208,7 +208,7 @@ public class MainController {
         try {
             byte[] payContractSignature = Abi.fromJson(App.getContractAbi()).get(4).fingerprintSignature();
             String txId = executeTransaction(TransactionType.CALL, 0, payContractSignature);
-            Thread.sleep(8000L);
+            Thread.sleep(1000L);
             return getTransactionOutput(txId);
         } catch (InterruptedException e){
             return "";
@@ -219,7 +219,7 @@ public class MainController {
         try {
             byte[] payContractSignature = Abi.fromJson(App.getContractAbi()).get(abiIndex).fingerprintSignature();
             String txId = executeTransaction(TransactionType.CALL, amount, payContractSignature);
-            Thread.sleep(8000L);
+            Thread.sleep(1000L);
             return getTransactionOutput(txId);
         } catch (InterruptedException e){
             return "";
@@ -231,7 +231,7 @@ public class MainController {
             Abi.Function c = (Abi.Function) Abi.fromJson(App.getContractAbi()).get(abiIndex);
             byte [] params = c.encode(new FixedNumber(param).getValue());
             String txId = executeTransaction(TransactionType.CALL, amount, params);
-            Thread.sleep(8000L);
+            Thread.sleep(2000L);
             return getTransactionOutput(txId);
         } catch (InterruptedException e){
             return "";
