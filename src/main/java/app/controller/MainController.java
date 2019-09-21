1c250c726164fd2952ab94dbc13c8e9afdea4a42
Artem <ae@atr0x.com>
2019-09-21 18:27:06 +0200

    debug

--- src/main/java/app/controller/MainController.java
+++ src/main/java/app/controller/MainController.java
@@ -49,11 +49,10 @@ public class MainController {
         HashMap<String, Object> displayTableMap =  transactionUtil.getTxById(tableTxId);
         while(displayTableMap.isEmpty() || displayTableMap.get("output").equals("") || displayTableMap.get("output") == null){
             Thread.sleep(1000L);
-            log.info("Table not there yet");
-            log.info(displayTableMap.toString());
-            log.info(tableTxId);
+            log.info("Table not there yet: " + tableTxId);
             displayTableMap = transactionUtil.getTxById(tableTxId);
         }
+        log.info(displayTableMap.toString());
         String tableEncoded = (String) displayTableMap.get("output");
         String msg = "Welcome to APEX BlackJack";
 
